d8b683ff3494320cb7d76985d34cb98bde814279
tomo621 <ktc25a32a0009@edu.kyoto-tech.ac.jp>
2026-06-13 14:08:11 +0900

    a

---
+++ ggggggggggggggggggggggggggggg.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <memory>
+
+class kadai
+{
+public:
+    // コンストラクタ
+    kadai()
+    {
+        std::cout << "生成" << std::endl;
+    }
+
+    // デストラクタ
+    ~kadai()
+    {
+        std::cout << "破棄" << std::endl;
+    }
+};
+
+// メイン関数
+int main()
+{
+    {
+        std::unique_ptr<kadai> ptr = std::make_unique<kadai>();
+    }
+    return 0;
+}
